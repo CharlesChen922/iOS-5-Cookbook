72bbd2b99d39cc4ab239ed14e28e5d9753c283c0
Yehnan Chiang <yehnan174@gmail.com>
2012-01-05 15:13:40 +0800

    translate comments to Chinese, C04/00-04-Converter Walkthrough Code

--- C04/00-04-Converter Walkthrough Code/Hello World/main.m
+++ C04/00-04-Converter Walkthrough Code/Hello World/main.m
@@ -8,19 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+// 好用的列按鈕項目建立巨集
 #define BARBUTTON(TITLE, SELECTOR)     [[UIBarButtonItem alloc] \
 initWithTitle:TITLE style:UIBarButtonItemStylePlain target:self \
 action:SELECTOR]
 
 @interface HelloWorldController : UIViewController 
 {
-    UITextField *field1;
-    UITextField *field2;
+    UITextField *field1; // Fahrenheit華氏
+    UITextField *field2; // Celsius攝氏
 }
 -(void) convert: (id)sender;
 @end
 
 @implementation HelloWorldController
+
+// 轉換成攝氏
 - (void) convert: (id) sender
 {
     float invalue = [[field1 text] floatValue];
@@ -31,10 +34,11 @@ action:SELECTOR]
 
 - (void)loadView
 {
+	// 建立視圖
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    // Establish two fields and two labels
+    // 建立兩個文字輸入欄位與兩個標籤
     field1 = [[UITextField alloc] initWithFrame:
               CGRectMake(185.0, 16.0, 97.0, 31.0)];
     field1.borderStyle = UITextBorderStyleRoundedRect;
@@ -58,18 +62,19 @@ action:SELECTOR]
     label2.textAlignment = UITextAlignmentLeft;
     label2.backgroundColor = [UIColor clearColor];
     
-    // Add items to content view
+    // 將各元件加入內容視圖裡
     [self.view addSubview:field1];
     [self.view addSubview:field2];
     [self.view addSubview:label1];
     [self.view addSubview:label2];
     
-    // Set title and add convert button
+    // 設定標題文字，加入Convert按鈕
     self.title = @"Converter";
     self.navigationItem.rightBarButtonItem = 
     BARBUTTON(@"Convert", @selector(convert:));
 }
 
+// 應用程式支援所有裝置擺設方向
 - (BOOL) shouldAutorotateToInterfaceOrientation:
     (UIInterfaceOrientation)toInterfaceOrientation
 {
