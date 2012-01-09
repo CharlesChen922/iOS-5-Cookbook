e66411cc3be481c8f36189c88bfce99cadc8d715
Yehnan Chiang <yehnan174@gmail.com>
2012-01-09 14:25:33 +0800

    translate comments to Chinese, C08/12-Circles

--- C08/12-Circles/main.m
+++ C08/12-Circles/main.m
@@ -14,16 +14,16 @@
 #define POINT(_INDEX_) [(NSValue *)[points objectAtIndex:_INDEX_] CGPointValue]
 
 #pragma mark Bezier Utilities
-// Get points from Bezier Curve
+// 從貝茲路徑裡取得點座標
 void getPointsFromBezier(void *info, const CGPathElement *element) 
 {
     NSMutableArray *bezierPoints = (__bridge NSMutableArray *)info;    
     
-    // Retrieve the path element type and its points
+    // 取得路徑元素型別與其上的點
     CGPathElementType type = element->type;
     CGPoint *points = element->points;
     
-    // Add the points if they're available (per type)
+    // 如果可用的話（根據型別），把點加入
     if (type != kCGPathElementCloseSubpath)
     {
         [bezierPoints addObject:VALUE(0)];
@@ -43,19 +43,19 @@ NSArray *pointsFromBezierPath(UIBezierPath *bpath)
 }
 
 #pragma mark Geometry Utilities
-// Return center of the given rect
+// 回傳給定矩形的中心點
 CGPoint getRectCenter(CGRect rect)
 {
 	return CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
 }
 
-// Build rect around a given center
+// 根據給定中心點建立矩形
 CGRect rectAroundCenter(CGPoint center, float dx, float dy)
 {
 	return CGRectMake(center.x - dx, center.y - dy, dx * 2, dy * 2);
 }
 
-// Return dot product of two vectors normalized
+// 兩個向量，正規化後，求出點積
 float dotproduct (CGPoint v1, CGPoint v2)
 {
 	float dot = (v1.x * v2.x) + (v1.y * v2.y);
@@ -66,7 +66,7 @@ float dotproduct (CGPoint v1, CGPoint v2)
 	return dot;
 }
 
-// Return distance between two points
+// 回傳兩點的距離
 float distance (CGPoint p1, CGPoint p2)
 {
 	float dx = p2.x - p1.x;
@@ -75,7 +75,7 @@ float distance (CGPoint p1, CGPoint p2)
 	return sqrt(dx*dx + dy*dy);
 }
 
-// Return a point with respect to a given origin
+// 根據給定的原點，回傳點座標
 CGPoint pointWithOrigin(CGPoint pt, CGPoint origin)
 {
 	return CGPointMake(pt.x - origin.x, pt.y - origin.y);
@@ -83,7 +83,7 @@ CGPoint pointWithOrigin(CGPoint pt, CGPoint origin)
 
 #pragma mark Circle Detection
 
-// Calculate and return least bounding rectangle
+// 計算並回傳最小的包圍矩形
 CGRect boundingRect(NSArray *points)
 {
 	CGRect rect = CGRectZero;
@@ -112,18 +112,19 @@ CGRect testForCircle(NSArray *points, NSDate *firstTouchDate)
 		return CGRectZero;
 	}
 	
-	// Test 1: duration tolerance
+	// 檢測1：在多短時間內必須完成手勢
 	float duration = [[NSDate date] timeIntervalSinceDate:firstTouchDate];
 	if (DEBUG) NSLog(@"Transit duration: %0.2f", duration);
         
         float maxDuration = 2.0f;
-        if (duration > maxDuration) // allows longer time for use in simulator
+        if (duration > maxDuration) // 在模擬器上，允許更長一點的時間
         {
             if (DEBUG) NSLog(@"Excessive touch duration: %0.2f seconds vs %0.1f seconds", duration, maxDuration);
             return CGRectZero;
         }
 	
-	// Test 2: The number of direction changes should be limited to near 4
+	// 檢測2：方向變化的次數
+	// 限制在4次左右
 	int inflections = 0;
 	for (int i = 2; i < (points.count - 1); i++)
 	{
@@ -142,7 +143,7 @@ CGRect testForCircle(NSArray *points, NSDate *firstTouchDate)
 		return CGRectZero;
 	}
 	
-	// Test 3: The start and end points must be between some number of points of each other
+	// 檢測3：起點與終點必須在一定程度內靠在一起
 	float tolerance = [[[UIApplication sharedApplication] keyWindow] bounds].size.width / 3.0f;	
 	if (distance(POINT(0), POINT(points.count - 1)) > tolerance)
 	{
@@ -150,7 +151,7 @@ CGRect testForCircle(NSArray *points, NSDate *firstTouchDate)
 		return CGRectZero;
 	}
 	
-	// Test 4: Count the distance traveled in degrees. 
+	// 檢測4：計算手勢劃過的角度
 	CGRect circle = boundingRect(points);
 	CGPoint center = getRectCenter(circle);
 	float distance = ABS(acos(dotproduct(pointWithOrigin(POINT(0), center), pointWithOrigin(POINT(1), center))));
@@ -159,16 +160,16 @@ CGRect testForCircle(NSArray *points, NSDate *firstTouchDate)
         
         float transitTolerance = distance - 2 * M_PI;
         
-        if (transitTolerance < 0.0f) // fell short of 2 PI
+        if (transitTolerance < 0.0f) // 小於2*PI
         {
-            if (transitTolerance < - (M_PI / 4.0f)) // 45 degrees or more
+            if (transitTolerance < - (M_PI / 4.0f)) // 45度或更大
             {
                 if (DEBUG) NSLog(@"Transit was too short, under 315 degrees");
                 return CGRectZero;
             }
         }
 	
-	if (transitTolerance > M_PI) // additional 180 degrees
+	if (transitTolerance > M_PI) // 多了180度以上
 	{
 		if (DEBUG) NSLog(@"Transit was too long, over 540 degrees");
 		return CGRectZero;
